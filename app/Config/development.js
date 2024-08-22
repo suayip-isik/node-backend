e768b28f50728567c5b867123ac9edd5255f4ed0
Şuayip Işık <suayip.isik.16@gmail.com>
2024-08-23 01:17:22 +0300

    added file size limit

--- app/Config/development.js
+++ app/Config/development.js
@@ -1,19 +1,15 @@
 import multer from "multer";
-import db from "../Models/index.js";
-
-const User = db.users;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, `app/uploads/data/${req.url.split("/")[1]}`);
   },
-  filename: async (req, file, cb) => {
+  filename: (req, file, cb) => {
     const { id } = req.params;
-    const user = await User.findByPk(id);
-    cb(null, user.userName + "." + file.originalname.split(".")[1]);
+    cb(null, id + "." + file.originalname.split(".")[1]);
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage, limits: { fileSize: 1024 * 1024 * 5 } });
 
 export { upload };
--- app/Routes/userRoutes.js
+++ app/Routes/userRoutes.js
@@ -8,6 +8,7 @@ import {
 } from "../Controllers/userController.js";
 import { saveUser } from "../Middleware/index.js";
 import { upload } from "../Config/index.js";
+import multer from "multer";
 
 const router = Router();
 
@@ -19,7 +20,18 @@ router.get("/getAllUsers", getUsers);
 
 router.post(
   "/uploadUserProfilePhoto/:id",
-  upload.single("file"),
+  function (req, res, next) {
+    upload.single("file")(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        console.log("girdi");
+        res.status(413).json({
+          success: false,
+          message: "Resim boyutu en fazla 5 MB olmalıdır.",
+        });
+      }
+      next();
+    });
+  },
   uploadUserProfilePhoto
 );
 
